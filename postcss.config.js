62b9a6bf203bb635707c8ec9da9f277b00d15727
Masato Nakamura <m.nakamura@madesignartworks.com>
2019-02-17 23:54:22 +0900

    postcssにautoprefixerを追記

--- postcss.config.js
+++ postcss.config.js
@@ -3,6 +3,7 @@ module.exports = {
         "postcss-cssnext": {},
         "cssnano": {
             preset: 'default'
-        }
+        },
+        autoprefixer: { browsers: ['last 2 versions', 'iOS >= 8'] }
     }
 }
